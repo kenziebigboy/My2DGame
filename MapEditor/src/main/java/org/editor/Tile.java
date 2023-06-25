b49cf0b0a6c347c98c182c94f1cb3e8026986637
mcbigboy <114679787+kenziebigboy@users.noreply.github.com>
2023-06-24 19:51:19 -0600

    Add the MapEditor to Game Gode Repo

---
+++ MapEditor/src/main/java/org/editor/Tile.java
@@ -0,0 +1,17 @@
+package org.editor;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    BufferedImage image;
+    ImageIcon icon;
+    boolean collision;
+    String name;
+}
+
+
+/* Location:              C:\Users\mcbig\Downloads\Simple2DTileEditor1.01(1)\!\com\simpleeditor\main\Tile.class
+ * Java compiler version: 12 (56.0)
+ * JD-Core Version:       1.1.3
+ */
