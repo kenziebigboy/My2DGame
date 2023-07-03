0f1c24630abd46054a3962a7e87153bf42ea3b0c
jeromekelley <jerome.kelley@mykenzie.snhu.edu>
2023-07-02 18:21:54 -0700

    dont know what this rebase problem is

--- Tile_Data_Manage/src/main/java/org/tilemanger/Main.java
+++ Tile_Data_Manage/src/main/java/org/tilemanger/Main.java
@@ -1,14 +1,20 @@
 package org.tilemanger;
 
+import org.tilemanger.panels.DisplayRawData;
 import org.tilemanger.panels.ImageProcessingPanel;
 import org.tilemanger.panels.PackageManagerPanel;
 import org.tilemanger.panels.PackagesPanel;
+import org.tilemanger.tables.GraphicsPackages;
+import org.tilemanger.tables.TileData;
+import org.tilemanger.tables.TileSheetData;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 
 public class Main extends JFrame {
+
+    public int selectedCol;
+    public int selectedRow;
 
     // Packages Panel
     PackagesPanel packagesPanel;
@@ -19,6 +25,8 @@ public class Main extends JFrame {
     // Tile Sheet Image Pre View
     public static ImageProcessingPanel imagePerViewPanel;
 
+    public DisplayRawData displayRawData = new DisplayRawData(this);
+
 
     public Main() {
 
@@ -26,15 +34,16 @@ public class Main extends JFrame {
         setLayout(null);
         MenuSystem();
 
-        this.packagesPanel = new PackagesPanel();
+        this.packagesPanel = new PackagesPanel(this);
         add(packagesPanel);
 
         this.packageManagerPanel = new PackageManagerPanel();
         add(packageManagerPanel);
 
-        this.imagePerViewPanel = new ImageProcessingPanel();
+        this.imagePerViewPanel = new ImageProcessingPanel(this);
         add(imagePerViewPanel);
 
+        add(displayRawData);
 
     }
 
@@ -48,43 +57,64 @@ public class Main extends JFrame {
         main.setLocationRelativeTo(null);
         Dimension mainScreenDim = new Dimension(1000,800);
         main.setPreferredSize(mainScreenDim);
+
+    }
+
+    public int getStartX(int panelWidth){
+        return (getWidth() - panelWidth) / 2;
+    }
+
+    public int getStartY(int panelHeight){
+        return (getHeight() - panelHeight) / 2;
     }
 
     private void MenuSystem(){
 
         JMenuBar mainBar    = new JMenuBar();
         JMenu sheetData     = new JMenu("Sheet Data");
+        JMenu rawdata       = new JMenu("Raw Data");
 
         // Adding menu to bar
         mainBar.add(sheetData);
+        mainBar.add(rawdata);
 
         // Create menu items for sheet data
         JMenuItem packages  = new JMenuItem("Packages");
-
         JMenuItem viewTileSheets = new JMenuItem("View TileSheets");
 
         // Add menu items to sheet Data
         sheetData.add(packages);
-
         sheetData.add(viewTileSheets);
+
+        // Create Menu Items for Raw Data
+        JMenuItem displayRawGraphicsPackageData = new JMenuItem("Graphics Package");
+        JMenuItem displayRawTileSheetData = new JMenuItem("Tile Sheet Data");
+        JMenuItem displayRawTileData = new JMenuItem("Tile Data");
+
+        // Add menu Items to Raw Data
+        rawdata.add(displayRawGraphicsPackageData);
+        rawdata.add(displayRawTileSheetData);
+        rawdata.add(displayRawTileData);
 
         setJMenuBar(mainBar);
 
         // Add Action Listeners for menus
-        packages.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        packages.addActionListener(e -> packagesPanel.displayPackagePanel());
 
-                packagesPanel.displayPackagePanel();
-            }
+        viewTileSheets.addActionListener(e -> {
+
         });
 
+        displayRawGraphicsPackageData.addActionListener(e -> {
+            displayRawData.displayRawGraphicsPackages(GraphicsPackages.getGraphicsPackagesList());
+        });
 
-        viewTileSheets.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        displayRawTileSheetData.addActionListener(e -> {
+            displayRawData.displayRawTileSheetData(TileSheetData.getTileSheetDataList());
+        });
 
-            }
+        displayRawTileData.addActionListener(e -> {
+            displayRawData.displayRawTileData(TileData.getTileDataList());
         });
 
 
